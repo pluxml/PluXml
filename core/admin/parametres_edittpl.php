3bac0666866d7218fce7ec89dfee15ab2cecab47
stephane <stephane@pluxml.org>
2012-09-11 16:27:19 +0200

    [+] Administration: Paramètres > Options d'affichage > Éditer les fichiers du thème : edition des fichiers dans les sous-dossiers

--- core/admin/parametres_edittpl.php
+++ core/admin/parametres_edittpl.php
@@ -41,11 +41,24 @@ if(isset($_POST['submit']) AND trim($_POST['content']) != '') {
 
 # On récupère les fichiers templates du thèmes
 $aTemplates=array();
-$files = plxGlob::getInstance(PLX_ROOT.$plxAdmin->aConf['racine_themes'].$style);
-if ($aTpls = $files->query('/[a-z0-9-_]+.(php|css)$/')) {
-	foreach($aTpls as $k=>$v)
-		$aTemplates[$v] = $v;
-}
+function listFolderFiles($dir, $include, $root=''){ 
+		$content = array();
+    $ffs = scandir($dir); 
+    foreach($ffs as $ff){ 
+				if($ff!='.' && $ff!='..') {  
+						$ext = strtolower(strrchr($ff,'.'));
+						if(!is_dir($dir.'/'.$ff) AND is_array($include) AND in_array($ext,$include)) {
+								$f = str_replace($root, "", PLX_ROOT.ltrim($dir.'/'.$ff,'./'));
+								$content[$f] = $f; 
+						}
+						if(is_dir($dir.'/'.$ff)) 
+								$content = array_merge($content, listFolderFiles($dir.'/'.$ff,$include,$root)); 
+				}  
+    } 
+    return $content;
+} 
+$root = PLX_ROOT.$plxAdmin->aConf['racine_themes'].$style;
+$aTemplates=listFolderFiles($root, array('.php','.css','.htm','.html','.txt','.js'), $root);
 
 # On récupère le contenu du fichier template
 $content = '';
