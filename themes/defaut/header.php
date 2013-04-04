5257422d8fed5ca5adba20610661879af132ee7e
Stéphane <stephane@pluxml.org>
2013-04-04 09:00:55 +0200

    fix #10: Thème par défaut: balise html absente

--- themes/defaut/header.php
+++ themes/defaut/header.php
@@ -1,6 +1,6 @@
 <?php if (!defined('PLX_ROOT')) exit; ?>
 <!DOCTYPE html>
-
+<html lang="<?php $plxShow->defaultLang() ?>">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
