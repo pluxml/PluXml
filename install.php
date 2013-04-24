f8c245be511e68a69d5d8018b929481d590e81b6
Stephane <stephane@pluxml.org>
2013-04-24 19:33:12 +0200

    fix #18: virgule oubliée

--- install.php
+++ install.php
@@ -111,7 +111,7 @@ $config = array('title'=>'PluXml',
 				'racine_themes'=>'themes/',
 				'racine_plugins'=>'plugins/',
 				'homestatic'=>'',
-				'hometemplate'=>'home.php'
+				'hometemplate'=>'home.php',
 				'urlrewriting'=>0,
 				'gzip'=>0,
 				'feed_chapo'=>0,
