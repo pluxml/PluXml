9f2a7f30bb961b020919574659415645e37f8964
Stephane <stephane@pluxml.org>
2013-10-17 11:52:48 +0200

    maj commentaires code

--- core/admin/parametres_plugincss.php
+++ core/admin/parametres_plugincss.php
@@ -17,8 +17,9 @@ $plxAdmin->checkProfil(PROFIL_ADMIN);
 $plugin = isset($_GET['p'])?urldecode($_GET['p']):'';
 $plugin = plxUtils::nullbyteRemove($plugin);
 
-# chargement du fichier css du plugin frontend
+# chargement du fichier css du plugin pour le frontend
 $file_frontend = PLX_ROOT.PLX_CONFIG_PATH.'plugins/'.basename($plugin).'.site.css';
+# chargement du fichier css du plugin pour le backend
 $file_backend = PLX_ROOT.PLX_CONFIG_PATH.'plugins/'.basename($plugin).'.admin.css';
 
 # Traitement du formulaire: sauvegarde du code css et regénération du cache
