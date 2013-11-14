20a7aec378c861f0f1a700269e3ce4a892a27bbd
Stephane <stephane@pluxml.org>
2013-11-14 09:55:53 +0100

    echo en trop

--- themes/defaut/footer.php
+++ themes/defaut/footer.php
@@ -9,7 +9,7 @@
 		<?php $plxShow->lang('POWERED_BY') ?> <a href="http://www.pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
 		<?php $plxShow->lang('IN') ?> <?php $plxShow->chrono(); ?>
 		<a rel="nofollow" href="<?php $plxShow->urlRewrite('core/admin/'); ?>" title="<?php $plxShow->lang('ADMINISTRATION') ?>"><?php $plxShow->lang('ADMINISTRATION') ?></a>
-		<a href="<?php echo $plxShow->urlRewrite('#top') ?>" title="<?php $plxShow->lang('GOTO_TOP') ?>"><?php $plxShow->lang('TOP') ?></a>
+		<a href="<?php $plxShow->urlRewrite('#top') ?>" title="<?php $plxShow->lang('GOTO_TOP') ?>"><?php $plxShow->lang('TOP') ?></a>
 		<?php $plxShow->httpEncoding() ?>
 	</p>
 
