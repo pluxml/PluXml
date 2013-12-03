3bcaa82b2ba226c771d6948cd91663865b66b078
Stephane <stephane@pluxml.org>
2013-12-03 11:16:01 +0100

    modif du viewport: ajout de user-scalable=yes

--- themes/defaut/header.php
+++ themes/defaut/header.php
@@ -3,7 +3,7 @@
 <html lang="<?php $plxShow->defaultLang() ?>">
 <head>
 <meta charset="<?php $plxShow->charset('min'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0">
 <title><?php $plxShow->pageTitle(); ?></title>
 <?php $plxShow->meta('description') ?>
 <?php $plxShow->meta('keywords') ?>
@@ -23,22 +23,22 @@
 
 <body id="top">
 
-	<nav role="navigation">
+    <nav role="navigation">
 
-		<ul id="static-nav">
-			<?php $plxShow->staticList($plxShow->getLang('HOME'),'<li id="#static_id"><a href="#static_url" class="#static_status" title="#static_name">#static_name</a></li>'); ?>
-			<?php $plxShow->pageBlog('<li id="#page_id"><a class="#page_status" href="#page_url" title="#page_name">#page_name</a></li>'); ?>
-		</ul>
+        <ul id="static-nav">
+            <?php $plxShow->staticList($plxShow->getLang('HOME'),'<li id="#static_id"><a href="#static_url" class="#static_status" title="#static_name">#static_name</a></li>'); ?>
+            <?php $plxShow->pageBlog('<li id="#page_id"><a class="#page_status" href="#page_url" title="#page_name">#page_name</a></li>'); ?>
+        </ul>
 
-	</nav>
+    </nav>
 
-	<header role="banner">
+    <header role="banner">
 
-		<h1>
-			<?php $plxShow->mainTitle('link'); ?>
-		</h1>
-		<h2>
-			<?php $plxShow->subTitle(); ?>
-		</h2>
+        <h1>
+            <?php $plxShow->mainTitle('link'); ?>
+        </h1>
+        <h2>
+            <?php $plxShow->subTitle(); ?>
+        </h2>
 
-	</header>
+    </header>
