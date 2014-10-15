d737fe2dc88ddce7f8a2c11069268e120c7f6088
Stephane <stephane@pluxml.org>
2014-10-15 08:31:57 +0200

    delete default theme folder

--- themes/defaut/js/html5ie.js
+++
@@ -1,7 +0,0 @@
-document.createElement("header");
-document.createElement("footer");
-document.createElement("section");
-document.createElement("aside");
-document.createElement("nav");
-document.createElement("article");
-document.createElement("figure");
