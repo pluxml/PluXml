6db67dd7fe8c9d377977c01a929e2655d219ea1a
Jos <jos@pluxml.org>
2014-10-23 11:29:39 +0200

    Modification des listes dans footer.php

--- themes/defaut/footer.php
+++ themes/defaut/footer.php
@@ -8,7 +8,7 @@
 
 					<div class="col sma-12 med-8 lrg-9">
 
-						<ul class="list-unstyled">
+						<ul class="unstyled-list">
 							<li>© 2014 <?php $plxShow->mainTitle('link'); ?> - 
 							<?php $plxShow->subTitle(); ?></li>
 							<li><?php $plxShow->lang('POWERED_BY') ?> <a href="http://www.pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
@@ -21,7 +21,7 @@
 
 					<div class="col sma-12 med-4 lrg-3">
 
-						<ul class="list-unstyled">
+						<ul class="unstyled-list">
 							<li><a href="<?php $plxShow->urlRewrite('feed.php?rss') ?>" title="<?php $plxShow->lang('ARTICLES_RSS_FEEDS'); ?>"><?php $plxShow->lang('ARTICLES'); ?></a></li>
 							<li><a href="<?php $plxShow->urlRewrite('feed.php?rss/commentaires'); ?>" title="<?php $plxShow->lang('COMMENTS_RSS_FEEDS') ?>"><?php $plxShow->lang('COMMENTS'); ?></a></li>
 							<li><a href="<?php $plxShow->urlRewrite('#top') ?>" title="<?php $plxShow->lang('GOTO_TOP') ?>"><?php $plxShow->lang('TOP') ?></a></li>
