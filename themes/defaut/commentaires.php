027c95a736fd372065083bb515ecd5d151f302cd
Jos <jos@pluxml.org>
2014-10-23 11:27:49 +0200

    Mise en place du nouveau système de formulaire sur le formulaire de commentaire

--- themes/defaut/commentaires.php
+++ themes/defaut/commentaires.php
@@ -38,29 +38,34 @@
 
 		<fieldset>
 
-			<div class="grid">
+			<div class="basic-form">
+				<label for="id_name"><?php $plxShow->lang('NAME') ?> :</label>
+				<input id="id_name" name="name" type="text" size="20" value="<?php $plxShow->comGet('name',''); ?>" maxlength="30" />
+			</div>
+			<div class="basic-form">
+				<label for="id_mail"><?php $plxShow->lang('EMAIL') ?> :</label>
+				<input id="id_mail" name="mail" type="text" size="20" value="<?php $plxShow->comGet('mail',''); ?>" />			
+			</div>
+			<div class="basic-form">
+				<label for="id_site"><?php $plxShow->lang('WEBSITE') ?> :</label>
+				<input id="id_site" name="site" type="text" size="20" value="<?php $plxShow->comGet('site',''); ?>" />
+			</div>
 
-				<div class="col sma-12 med-6">
-					<label for="id_name"><?php $plxShow->lang('NAME') ?> :</label>
-					<input id="id_name" name="name" type="text" size="20" value="<?php $plxShow->comGet('name',''); ?>" maxlength="30" />
-					<label for="id_mail"><?php $plxShow->lang('EMAIL') ?> :</label>
-					<input id="id_mail" name="mail" type="text" size="20" value="<?php $plxShow->comGet('mail',''); ?>" />			
-					<label for="id_site"><?php $plxShow->lang('WEBSITE') ?> :</label>
-					<input id="id_site" name="site" type="text" size="20" value="<?php $plxShow->comGet('site',''); ?>" />
-				</div>
-				<div class="col sma-12 med-6">
-					<label for="id_content" class="lab_com"><?php $plxShow->lang('COMMENT') ?> :</label>
-					<textarea id="id_content" name="content" cols="35" rows="6"><?php $plxShow->comGet('content',''); ?></textarea>
-				</div>
+			<div class="basic-form">
+				<label for="id_content" class="lab_com"><?php $plxShow->lang('COMMENT') ?> :</label>
+				<textarea id="id_content" name="content" cols="35" rows="6"><?php $plxShow->comGet('content',''); ?></textarea>
 			</div>
 
 			<?php $plxShow->comMessage('<p class="text-red"><strong>#com_message</strong></p>'); ?>
 			
 			<?php if($plxShow->plxMotor->aConf['capcha']): ?>
+
+			<div class="inline-form">
 			<label for="id_rep"><strong><?php echo $plxShow->lang('ANTISPAM_WARNING') ?></strong></label>
 			<?php $plxShow->capchaQ(); ?> :
 			<input id="id_rep" name="rep" type="text" size="2" maxlength="1" style="width: auto; display: inline;" />
 			<?php endif; ?>
+			</div>
 			<input type="submit" value="<?php $plxShow->lang('SEND') ?>" />
 
 		</fieldset>
