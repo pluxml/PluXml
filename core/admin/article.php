5bdef5aa46516b31ddf7048a39c70673ada47061
Stephane <stephane@pluxml.org>
2014-12-18 15:57:40 +0100

    ajustement taille du champ Lien article

--- core/admin/article.php
+++ core/admin/article.php
@@ -292,8 +292,8 @@ $cat_id='000';
 				</div>
 				<div class="inline-form">
 					<label for="id_link"><?php echo L_LINK_FIELD ?>&nbsp;:&nbsp;</label>
-					<?php echo '<input id="id_link" onclick="this.select()" class="readonly" readonly="readonly" type="text" value="'.$link.'" />' ?>
 					<?php echo '<a onclick="this.target=\'_blank\';return true;" href="'.$link.'" title="'.L_LINK_ACCESS.'">'.L_LINK_VIEW.'</a>'; ?>
+					<?php echo '<input id="id_link" onclick="this.select()" class="readonly" readonly="readonly" type="text" value="'.$link.'" />' ?>
 					<?php endif; ?>
 				</div>
 			</fieldset>
