91ad6a1890dcc2390dca72a9044eef403c262ad0
Jos <jos@pluxml.org>
2015-01-22 09:26:00 +0100

    Ajout d'un favicon dans l'admin (problème avec le gif)

--- core/admin/top.php
+++ core/admin/top.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=<?php echo strtolower(PLX_CHARSET) ?>" />
     <link rel="stylesheet" type="text/css" href="<?php echo PLX_CORE ?>admin/theme/plucss.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="<?php echo PLX_CORE ?>admin/theme/theme.css" media="screen" />
-    <link rel="icon" href="<?php echo PLX_CORE ?>admin/theme/images/pluxml.gif" />
+    <link rel="icon" href="<?php echo PLX_CORE ?>admin/theme/images/favicon.png" />
 	<?php if(is_file(PLX_ROOT.$plxAdmin->aConf['custom_admincss_file'])) echo '<link rel="stylesheet" type="text/css" href="'.PLX_ROOT.$plxAdmin->aConf['custom_admincss_file'].'" media="screen" />'."\n" ?>
     <?php
     if(file_exists(PLX_ROOT.$plxAdmin->aConf['racine_plugins'].'admin.css'))
