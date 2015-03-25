282b4a8fd87315dc3d47ddbb05f7bf2ff13d7f2d
Jos <jos@pluxml.org>
2015-03-25 11:42:09 +0100

    Thème : mise à jour PluCSS + Formulaire + theme.css

--- themes/defaut/commentaires.php
+++ themes/defaut/commentaires.php
@@ -38,35 +38,46 @@
 
 		<fieldset>
 
-			<div class="basic-form">
-				<label for="id_name"><?php $plxShow->lang('NAME') ?> :</label>
-				<input id="id_name" name="name" type="text" size="20" value="<?php $plxShow->comGet('name',''); ?>" maxlength="30" />
+			<div class="grid">
+				<div class="col sml-12">
+					<label for="id_name"><?php $plxShow->lang('NAME') ?> :</label>
+					<input id="id_name" name="name" type="text" size="20" value="<?php $plxShow->comGet('name',''); ?>" maxlength="30" />
+				</div>
 			</div>
-			<div class="basic-form">
-				<label for="id_mail"><?php $plxShow->lang('EMAIL') ?> :</label>
-				<input id="id_mail" name="mail" type="text" size="20" value="<?php $plxShow->comGet('mail',''); ?>" />			
+			<div class="grid">
+				<div class="col sml-12 lrg-6">
+					<label for="id_mail"><?php $plxShow->lang('EMAIL') ?> :</label>
+					<input id="id_mail" name="mail" type="text" size="20" value="<?php $plxShow->comGet('mail',''); ?>" />			
+				</div>
+				<div class="col sml-12 lrg-6">
+					<label for="id_site"><?php $plxShow->lang('WEBSITE') ?> :</label>
+					<input id="id_site" name="site" type="text" size="20" value="<?php $plxShow->comGet('site',''); ?>" />
+				</div>
 			</div>
-			<div class="basic-form">
-				<label for="id_site"><?php $plxShow->lang('WEBSITE') ?> :</label>
-				<input id="id_site" name="site" type="text" size="20" value="<?php $plxShow->comGet('site',''); ?>" />
-			</div>
-
-			<div class="basic-form">
-				<label for="id_content" class="lab_com"><?php $plxShow->lang('COMMENT') ?> :</label>
-				<textarea id="id_content" name="content" cols="35" rows="6"><?php $plxShow->comGet('content',''); ?></textarea>
+			<div class="grid">
+				<div class="col sml-12">
+					<label for="id_content" class="lab_com"><?php $plxShow->lang('COMMENT') ?> :</label>
+					<textarea id="id_content" name="content" cols="35" rows="6"><?php $plxShow->comGet('content',''); ?></textarea>
+				</div>
 			</div>
 
 			<?php $plxShow->comMessage('<p class="text-red"><strong>#com_message</strong></p>'); ?>
-			
+				
 			<?php if($plxShow->plxMotor->aConf['capcha']): ?>
 
-			<div class="basic-form">
-				<label for="id_rep"><strong><?php echo $plxShow->lang('ANTISPAM_WARNING') ?></strong></label>
-				<?php $plxShow->capchaQ(); ?>
-				<input id="id_rep" name="rep" type="text" size="2" maxlength="1" style="width: auto; display: inline;" />
-				<?php endif; ?>
+			<div class="grid">
+				<div class="col sml-12">
+					<label for="id_rep"><strong><?php echo $plxShow->lang('ANTISPAM_WARNING') ?></strong></label>
+					<?php $plxShow->capchaQ(); ?>
+					<input id="id_rep" name="rep" type="text" size="2" maxlength="1" style="width: auto; display: inline;" />
+					<?php endif; ?>
+				</div>
 			</div>
-			<input class="blue" type="submit" value="<?php $plxShow->lang('SEND') ?>" />
+			<div class="grid">
+				<div class="col sml-12">
+					<input class="blue" type="submit" value="<?php $plxShow->lang('SEND') ?>" />
+				</div>
+			</div>
 
 		</fieldset>
 			
@@ -74,8 +85,8 @@
 
 	<?php else: ?>
 
-		<p>
-			<?php $plxShow->lang('COMMENTS_CLOSED') ?>.
-		</p>
+	<p>
+		<?php $plxShow->lang('COMMENTS_CLOSED') ?>.
+	</p>
 
 	<?php endif; # Fin du if sur l'autorisation des commentaires ?>
