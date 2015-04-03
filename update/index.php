6db6c8be4939c05abf8a706ca331d1cbe1ca9199
Jos <jos@pluxml.org>
2015-04-03 11:00:04 +0200

    Admin : correction mise en page "page update"

--- update/index.php
+++ update/index.php
@@ -70,14 +70,14 @@ session_start();
 # Control du token du formulaire
 plxToken::validateFormToken($_POST);
 ?>
+
 <!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang ?>" lang="<?php echo $lang ?>">
 <head>
 	<meta name="robots" content="noindex, nofollow" />
+	<meta charset="<?php echo strtolower(PLX_CHARSET) ?>" />
 	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0">
 	<title><?php echo L_UPDATE_TITLE.' '.plxUtils::strCheck($plxUpdater->newVersion) ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo strtolower(PLX_CHARSET) ?>" />
     <link rel="stylesheet" type="text/css" href="<?php echo PLX_CORE ?>admin/theme/plucss.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="<?php echo PLX_CORE ?>admin/theme/theme.css" media="screen" />
     <link rel="icon" href="<?php echo PLX_CORE ?>admin/theme/images/pluxml.gif" />
@@ -85,63 +85,66 @@ plxToken::validateFormToken($_POST);
 
 <body>
 
-<main class="main grid">
+	<main class="main grid">
 
-	<aside class="aside col sml-12 med-3 lrg-2">
+		<aside class="aside col sml-12 med-3 lrg-2">
 
-	</aside>
+		</aside>
 
-	<section class="section col sml-12 med-9 lrg-10" style="margin-top: 0">
+		<section class="section col sml-12 med-9 med-offset-3 lrg-10 lrg-offset-2" style="margin-top: 0">
 
-		<header>
-			<h1><?php echo L_UPDATE_TITLE.' '.plxUtils::strCheck($plxUpdater->newVersion) ?></h1>
-		</header>
+			<header>
 
-		<?php if(empty($_POST['submit'])) : ?>
-			<?php if($plxUpdater->oldVersion==$plxUpdater->newVersion) : ?>
-			<p><strong><?php echo L_UPDATE_UPTODATE ?></strong></p>
-			<p><?php echo L_UPDATE_NOT_AVAILABLE ?></p>
-			<p><a href="<?php echo PLX_ROOT; ?>" title="<?php echo L_UPDATE_BACK ?>"><?php echo L_UPDATE_BACK ?></a></p>
+				<h1><?php echo L_UPDATE_TITLE.' '.plxUtils::strCheck($plxUpdater->newVersion) ?></h1>
+
+			</header>
+
+			<?php if(empty($_POST['submit'])) : ?>
+				<?php if($plxUpdater->oldVersion==$plxUpdater->newVersion) : ?>
+				<p><strong><?php echo L_UPDATE_UPTODATE ?></strong></p>
+				<p><?php echo L_UPDATE_NOT_AVAILABLE ?></p>
+				<p><a href="<?php echo PLX_ROOT; ?>" title="<?php echo L_UPDATE_BACK ?>"><?php echo L_UPDATE_BACK ?></a></p>
+				<?php else: ?>
+				<form action="index.php" method="post">
+					<fieldset>
+						<div class="grid">
+							<div class="col sml-12 med-5 label-centered">
+								<label for="id_default_lang"><?php echo L_SELECT_LANG ?></label>
+							</div>
+							<div class="col sml-12 med-7">
+								<?php plxUtils::printSelect('default_lang', plxUtils::getLangs(), $lang) ?>&nbsp;
+							</div>
+						</div>
+						<div class="grid">
+							<div class="col sml-12">
+								<input type="submit" name="select_lang" value="<?php echo L_INPUT_CHANGE ?>" />
+								<?php echo plxToken::getTokenPostMethod() ?>
+							</div>
+						</div>
+					</fieldset>
+					<fieldset>
+						<p><strong><?php echo L_UPDATE_WARNING1.' '.$plxUpdater->oldVersion ?></strong></p>
+						<?php if(empty($plxUpdater->oldVersion)) : ?>
+						<p><?php echo L_UPDATE_SELECT_VERSION ?></p>
+						<p><?php plxUtils::printSelect('version',array_keys($versions),''); ?></p>
+						<p><?php echo L_UPDATE_WARNING2 ?></p>
+						<?php endif; ?>
+						<p><?php echo L_UPDATE_WARNING3 ?></p>
+						<p><input type="submit" name="submit" value="<?php echo L_UPDATE_START ?>" /></p>
+					</fieldset>
+				</form>
+				<?php endif; ?>
 			<?php else: ?>
-			<form action="index.php" method="post">
-				<fieldset>
-					<div class="grid">
-						<div class="col sml-12 med-5">
-							<label for="id_default_lang"><?php echo L_SELECT_LANG ?></label>
-						</div>
-						<div class="col sml-12 med-7">
-							<?php plxUtils::printSelect('default_lang', plxUtils::getLangs(), $lang) ?>&nbsp;
-						</div>
-					</div>
-					<div class="grid">
-						<div class="col sml-12">
-							<input type="submit" name="select_lang" value="<?php echo L_INPUT_CHANGE ?>" />
-							<?php echo plxToken::getTokenPostMethod() ?>
-						</div>
-					</div>
-				</fieldset>
-				<fieldset>
-					<p><strong><?php echo L_UPDATE_WARNING1.' '.$plxUpdater->oldVersion ?></strong></p>
-					<?php if(empty($plxUpdater->oldVersion)) : ?>
-					<p><?php echo L_UPDATE_SELECT_VERSION ?></p>
-					<p><?php plxUtils::printSelect('version',array_keys($versions),''); ?></p>
-					<p><?php echo L_UPDATE_WARNING2 ?></p>
-					<?php endif; ?>
-					<p><?php echo L_UPDATE_WARNING3 ?></p>
-					<p><input type="submit" name="submit" value="<?php echo L_UPDATE_START ?>" /></p>
-				</fieldset>
-			</form>
+			<?php
+			$version = isset($_POST['version']) ? $_POST['version'] : $plxUpdater->oldVersion;
+			$plxUpdater->startUpdate($version);
+			?>
+			<p><a href="<?php echo PLX_ROOT; ?>" title="<?php echo L_UPDATE_BACK ?>"><?php echo L_UPDATE_BACK ?></a></p>
 			<?php endif; ?>
-		<?php else: ?>
-		<?php
-		$version = isset($_POST['version']) ? $_POST['version'] : $plxUpdater->oldVersion;
-		$plxUpdater->startUpdate($version);
-		?>
-		<p><a href="<?php echo PLX_ROOT; ?>" title="<?php echo L_UPDATE_BACK ?>"><?php echo L_UPDATE_BACK ?></a></p>
-		<?php endif; ?>
-	</section>
+		</section>
 
-</main>
+	</main>
 
 </body>
+
 </html>
