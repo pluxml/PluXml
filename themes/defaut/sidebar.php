059ca410466087157048590f5d6bdd57a5c0f041
Stephane <stephane@pluxml.org>
2015-06-16 10:19:09 +0200

    ajout d'une classe pour identifier chaque bloc

--- themes/defaut/sidebar.php
+++ themes/defaut/sidebar.php
@@ -6,7 +6,7 @@
 			<?php $plxShow->lang('CATEGORIES'); ?>
 		</h3>
 
-		<ul class="unstyled-list">
+		<ul class="cat-list unstyled-list">
 			<?php $plxShow->catList('','<li id="#cat_id"><a class="#cat_status" href="#cat_url" title="#cat_name">#cat_name</a> (#art_nb)</li>'); ?>
 		</ul>
 
@@ -14,7 +14,7 @@
 			<?php $plxShow->lang('LATEST_ARTICLES'); ?>
 		</h3>
 
-		<ul class="unstyled-list">
+		<ul class="lastart-list unstyled-list">
 			<?php $plxShow->lastArtList('<li><a class="#art_status" href="#art_url" title="#art_title">#art_title</a></li>'); ?>
 		</ul>
 
@@ -30,7 +30,7 @@
 			<?php $plxShow->lang('LATEST_COMMENTS'); ?>
 		</h3>
 
-		<ul class="unstyled-list">
+		<ul class="lastcom-list unstyled-list">
 			<?php $plxShow->lastComList('<li><a href="#com_url">#com_author '.$plxShow->getLang('SAID').' : #com_content(34)</a></li>'); ?>
 		</ul>
 
@@ -38,14 +38,14 @@
 			<?php $plxShow->lang('ARCHIVES'); ?>
 		</h3>
 
-		<ul class="unstyled-list">
+		<ul class="arch-list unstyled-list">
 			<?php $plxShow->archList('<li id="#archives_id"><a class="#archives_status" href="#archives_url" title="#archives_name">#archives_name</a> (#archives_nbart)</li>'); ?>
 		</ul>
 
 		<h3>
 			RSS
 		</h3>
-			<ul class="unstyled-list">
+			<ul class="rss-list unstyled-list">
 				<li><a href="<?php $plxShow->urlRewrite('feed.php?rss') ?>" title="<?php $plxShow->lang('ARTICLES_RSS_FEEDS'); ?>"><?php $plxShow->lang('ARTICLES'); ?></a></li>
 				<li><a href="<?php $plxShow->urlRewrite('feed.php?rss/commentaires'); ?>" title="<?php $plxShow->lang('COMMENTS_RSS_FEEDS') ?>"><?php $plxShow->lang('COMMENTS'); ?></a></li>
 			</ul>
