d5970a15f7e85589c2a6d9daeceab4be86213acc
Jos <jos@pluxml.org>
2016-11-30 11:57:23 +0100

    Theme modification nav et header

--- themes/defaut/header.php
+++ themes/defaut/header.php
@@ -22,9 +22,9 @@
 	<nav class="nav" role="navigation">
 		<div class="container">
 			<div class="responsive-menu">
-				<label for="menu"><?php $plxShow->lang('MENU'); ?></label>
+				<label for="menu"></label>
 				<input type="checkbox" id="menu">
-				<ul class="menu sml-float-left med-float-right">
+				<ul class="menu">
 					<?php $plxShow->staticList($plxShow->getLang('HOME'),'<li class="#static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
 					<?php $plxShow->pageBlog('<li id="#page_id"><a class="#page_status" href="#page_url" title="#page_name">#page_name</a></li>'); ?>
 				</ul>
@@ -34,7 +34,7 @@
 
 	<header class="header" role="banner">
 		<div class="container">
-			<h1 class="no-margin"><?php $plxShow->mainTitle('link'); ?></h1>
+			<h1 class="no-margin heading-small"><?php $plxShow->mainTitle('link'); ?></h1>
 			<h2 class="h5 no-margin"><?php $plxShow->subTitle(); ?></h2>
 		</div>
 	</header>
