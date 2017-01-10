07236cdf975b07802c8fe5230bd0a40d76a24818
Stephane <sfer.online@free.fr>
2017-01-10 14:44:41 +0100

    reduction taille nom du site

--- core/admin/top.php
+++ core/admin/top.php
@@ -41,7 +41,7 @@
 				</li>
 			</ul>
 			<ul class="unstyled-list profil no-margin">
-				<li><h1 class="h4 no-margin site-name"><strong><?php echo plxUtils::strCheck($plxAdmin->aConf['title']) ?></strong></h1></li>
+				<li><h1 class="h5 no-margin site-name"><strong><?php echo plxUtils::strCheck($plxAdmin->aConf['title']) ?></strong></h1></li>
 				<li>
 					<strong><?php echo plxUtils::strCheck($plxAdmin->aUsers[$_SESSION['user']]['name']) ?></strong>&nbsp;:
 					<em>
