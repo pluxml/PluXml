a4919379a92a2324d7ee2f0bfaeeeeadd615324f
bazooka07 <jpourrez@gmail.com>
2017-09-10 15:21:43 +0200

    M.à.j. executables Linux

---
+++ build-zip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git archive -o ../PluXml-trans-$(date +%F).zip --prefix PluXml/ HEAD
+
--- core/admin/theme/flags/build.sh
+++ core/admin/theme/flags/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 # ImageMagick
-for rep in 32 48; do
+for rep in $(ls -d); do
 	convert "$rep/*.png" -append "flags-$rep.png"
 done
+
+cd ${OLDPWD}
