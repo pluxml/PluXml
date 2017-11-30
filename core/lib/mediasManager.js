63e8d67d711dab06d69894aac97378059ae9a603
Stephane <stephane@pluxml.org>
2017-11-30 19:30:49 +0000

    Gestion des régles CSS dans mediasManager

--- core/lib/mediasManager.js
+++ core/lib/mediasManager.js
@@ -23,17 +23,18 @@ var mediasManager = {
 	addText: function(cibleId, txt, replace) {
 		var txt = txt.replace(this.opts.racine, '');
 		var cible = window.opener.document.getElementById(cibleId);
-		if(cible) {
+		if (cible) {
 			cible.focus();
-			if(replace) {
+			if (replace) {
 				cible.value = txt;
-			} else {
-				if(window.opener.document.selection && window.opener.document.selection.createRange) {
+			}
+			else {
+				if (window.opener.document.selection && window.opener.document.selection.createRange) {
 					sel = window.opener.document.selection.createRange();
 					sel.text = sel.text + txt;
 				}
 				// Moz support
-				else if(cible.selectionStart || cible.selectionStart == '0') {
+				else if (cible.selectionStart || cible.selectionStart == '0') {
 					var startPos = cible.selectionStart;
 					var endPos = cible.selectionEnd;
 					var restoreTop = cible.scrollTop;
@@ -47,16 +48,17 @@ var mediasManager = {
 				}
 			}
 			cible.focus();
-		} else {
-			console.log('Element #'+cibleId+' introuvable - ' + txt);
+		}
+		else {
+			console.log('Element #' + cibleId + ' introuvable - ' + txt);
 		}
 		return false;
 	},
 
 	updImg: function(cibleId, imgPath) {
 		var id = window.opener.document.getElementById(cibleId);
-		if(id) {
-			id.innerHTML = '<img src="'+imgPath+'" alt="" />';
+		if (id) {
+			id.innerHTML = '<img src="' + imgPath + '" alt="" />';
 		}
 	},
 
@@ -64,7 +66,7 @@ var mediasManager = {
 
 		this.opts = options;
 
-		if(window.name == this.opts.windowName) {
+		if (window.name == this.opts.windowName) {
 
 			// ajout d'une class à <body> pour masquer les parties inutiles du gestionnaire de médias
 			document.body.classList.add('mediasManager');
@@ -72,11 +74,11 @@ var mediasManager = {
 			// ajout des évenements onclick pour récuper le lien de l'image
 			var tbody = document.querySelector('#medias-table tbody');
 			if (tbody) {
-				tbody.addEventListener('click', function (event) {
+				tbody.addEventListener('click', function(event) {
 					var target = event.target;
 					if (target.tagName == 'A') {
 						event.preventDefault();
-						var	launcher = window.opener.mediasManager;
+						var launcher = window.opener.mediasManager;
 						var replace = launcher.replace;
 						var cibleId = launcher.cibleId;
 						var fallback = launcher.fallback;
@@ -84,9 +86,10 @@ var mediasManager = {
 						if (typeof fn === "function") {
 							var fnparams = [cibleId, target.href, replace];
 							fn.apply(null, fnparams);
-						} else {
+						}
+						else {
 							mediasManager.addText(cibleId, target.href, replace);
-							mediasManager.updImg(cibleId+'_img', target.href);
+							mediasManager.updImg(cibleId + '_img', target.href);
 						}
 						window.close();
 						cibleId.focus();
@@ -97,19 +100,20 @@ var mediasManager = {
 	},
 
 	openPopup: function(cibleId, replace, fallback) {
-		var replace = replace==undefined ? false : true;
+		var replace = replace == undefined ? false : true;
 		var width = this.opts.width ? this.opts.width : 950;
 		var height = this.opts.height ? this.opts.height : 580;
 		var left = parseInt((screen.width - width) / 2);
 		var top = parseInt((screen.height - height) / 2);
-		var options = 'directories=no, toolbar=no, menubar=no, location=no, resizable=yes, scrollbars=yes, width='+width+' , height='+height+', left='+left+', top='+top;
-		this.cibleId=cibleId;
-		this.replace=replace;
-		this.fallback=fallback;
+		var options = 'directories=no, toolbar=no, menubar=no, location=no, resizable=yes, scrollbars=yes, width=' + width + ' , height=' + height + ', left=' + left + ', top=' + top;
+		this.cibleId = cibleId;
+		this.replace = replace;
+		this.fallback = fallback;
 		popup = window.open(unescape(this.opts.racine + this.opts.urlManager), this.opts.windowName, options);
-		if(popup) {
+		if (popup) {
 			popup.focus();
-		} else {
+		}
+		else {
 			alert('Ouverture de la fenêtre bloquée par un anti-popup!');
 		}
 		return false;
