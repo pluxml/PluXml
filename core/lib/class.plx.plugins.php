3dcf295b516d5e3f1a605d75d8d74246e01b2859
sudwebdesign <thomas@sudwebdesign.fr>
2018-03-17 16:40:08 +0100

    oups

--- core/lib/class.plx.plugins.php
+++ core/lib/class.plx.plugins.php
@@ -91,8 +91,8 @@ class plxCorePlugins  {
 								if(is_file(PLX_PLUGINS.$name.'/update')) {
 									# on supprime le fichier update pour eviter d'appeler la methode onUpdate
 									# à chaque chargement du plugin
-									@chmod(PLX_PLUGINS.$name.'/update', 0644);
-									@unlink(PLX_PLUGINS.$name.'/update');
+									chmod(PLX_PLUGINS.$name.'/update', 0644);
+									unlink(PLX_PLUGINS.$name.'/update');
 									$updAction = $instance->onUpdate();
 								}
 							}
