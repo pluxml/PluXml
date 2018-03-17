b423cac97fe4fc4b925f143fac978f73bbd4152c
sudwebdesign <thomas@sudwebdesign.fr>
2018-03-17 18:57:01 +0100

    Clean

--- core/admin/parametres_plugins.php
+++ core/admin/parametres_plugins.php
@@ -14,7 +14,7 @@ plxToken::validateFormToken($_POST);
 
 # Control de l'accès à la page en fonction du profil de l'utilisateur connecté
 $plxAdmin->checkProfil(PROFIL_ADMIN);
-function filterPlug($v){//si ds plugin.xml le scope est a site $v == false ??? var_dump($v);
+function filterPlug($v){
  return $v->CORE === 'plugin';
 }
 $aPlugins = array_filter($plxAdmin->plxPlugins->aPlugins, 'filterPlug');
