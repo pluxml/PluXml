440421d94a1536c6880b6e736a64b17fdac756a7
P3ter <contact@p3ter.fr>
2019-01-26 18:05:39 +0100

    Correction dans un commentaire

--- core/lib/class.plx.admin.php
+++ core/lib/class.plx.admin.php
@@ -284,7 +284,7 @@ RewriteRule ^feed\/(.*)$ feed.php?$1 [L]
 	/**
 	 * Méthode qui génère un nouveau mot de passe et envoi un mail à l'utilisateur
 	 *
-	 * @param   id_mail        identifiant de l'utilisateur
+	 * @param   id_mail     login ou adresse e-mail de l'utilisateur
 	 * @return	string      le nouveau mot de passe
 	 * @author	Pedro "P3ter" CADETE
 	 **/
