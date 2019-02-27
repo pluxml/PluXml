ce9934ecd8b4c7c7d583c851d8963ea309e8b50a
P3ter <contact@p3ter.fr>
2019-02-27 23:51:43 +0100

    Templating liste des articles

--- core/admin/articles.php
+++ core/admin/articles.php
@@ -129,26 +129,28 @@ include __DIR__ .'/top.php';
 
 <form action="articles.php" method="post" id="form_articles">
 
-    <div class="inline-form">
-		<ul class="menu">
-    		<li><a <?php echo ($_SESSION['sel_get']=='all')?'class="selected" ':'' ?>href="articles.php?sel=all&amp;page=1"><?php echo L_ALL ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('all', $userId).')' ?></li>
-    		<li><a <?php echo ($_SESSION['sel_get']=='published')?'class="selected" ':'' ?>href="articles.php?sel=published&amp;page=1"><?php echo L_ALL_PUBLISHED ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('published', $userId, '').')' ?></li>
-    		<li><a <?php echo ($_SESSION['sel_get']=='draft')?'class="selected" ':'' ?>href="articles.php?sel=draft&amp;page=1"><?php echo L_ALL_DRAFTS ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('draft', $userId).')' ?></li>
-    		<li><a <?php echo ($_SESSION['sel_get']=='mod')?'class="selected" ':'' ?>href="articles.php?sel=mod&amp;page=1"><?php echo L_ALL_AWAITING_MODERATION ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('all', $userId, '_').')' ?></li>
-		</ul>
+    <div class="panel  panel-content">
+        <div class="inline-form">
+    		<ul class="menu">
+        		<li><a <?php echo ($_SESSION['sel_get']=='all')?'class="selected" ':'' ?>href="articles.php?sel=all&amp;page=1"><?php echo L_ALL ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('all', $userId).')' ?></li>
+        		<li><a <?php echo ($_SESSION['sel_get']=='published')?'class="selected" ':'' ?>href="articles.php?sel=published&amp;page=1"><?php echo L_ALL_PUBLISHED ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('published', $userId, '').')' ?></li>
+        		<li><a <?php echo ($_SESSION['sel_get']=='draft')?'class="selected" ':'' ?>href="articles.php?sel=draft&amp;page=1"><?php echo L_ALL_DRAFTS ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('draft', $userId).')' ?></li>
+        		<li><a <?php echo ($_SESSION['sel_get']=='mod')?'class="selected" ':'' ?>href="articles.php?sel=mod&amp;page=1"><?php echo L_ALL_AWAITING_MODERATION ?></a><?php echo '&nbsp;('.$plxAdmin->nbArticles('all', $userId, '_').')' ?></li>
+    		</ul>
+        </div>
+        
+        <div class="grid ">
+        	<div class="col sml-5">
+        		<?php plxUtils::printSelect('sel_cat', $aFilterCat, $_SESSION['sel_cat']) ?>
+        		<input class="<?php echo $_SESSION['sel_cat']!='all'?' select':'' ?>" type="submit" value="<?php echo L_ARTICLES_FILTER_BUTTON ?>" />
+        	</div>
+        	<div class="col sml-7 text-right">
+        		<input id="articles-search" placeholder="<?php echo L_SEARCH_PLACEHOLDER ?>" type="text" name="artTitle" value="<?php echo plxUtils::strCheck($_GET['artTitle']) ?>" />
+        		<input class="<?php echo (!empty($_GET['artTitle'])?' select':'') ?>" type="submit" value="<?php echo L_SEARCH ?>" />
+        	</div>
+        </div>
     </div>
-    
-    <div class="grid">
-    	<div class="col sml-5">
-    		<?php plxUtils::printSelect('sel_cat', $aFilterCat, $_SESSION['sel_cat']) ?>
-    		<input class="<?php echo $_SESSION['sel_cat']!='all'?' select':'' ?>" type="submit" value="<?php echo L_ARTICLES_FILTER_BUTTON ?>" />
-    	</div>
-    	<div class="col sml-7 text-right">
-    		<input id="articles-search" placeholder="<?php echo L_SEARCH_PLACEHOLDER ?>" type="text" name="artTitle" value="<?php echo plxUtils::strCheck($_GET['artTitle']) ?>" />
-    		<input class="<?php echo (!empty($_GET['artTitle'])?' select':'') ?>" type="submit" value="<?php echo L_SEARCH ?>" />
-    	</div>
-    </div>
-    
+
     <div class="scrollable-table panel">
     	<div class="panel-content">
     		<h3 class="no-margin"><?php echo L_ARTICLES_LIST ?></h3>
@@ -227,55 +229,60 @@ include __DIR__ .'/top.php';
         		?>
     		</tbody>
     	</table>
-    	<div class="panel-content">
-        	<?php
-                echo plxToken::getTokenPostMethod();
-                if($_SESSION['profil']<=PROFIL_MODERATOR) {
-                    echo '<input type="submit" name="delete" value="'.L_DELETE.'" onclick="Check=confirm(\''.L_ARTICLE_DELETE_CONFIRM.'\');if(Check==false) {return false;} else {this.form.target=\'_self\';return true;}" /> ';
-                }
-            ?>
-            <?php plxUtils::printInput('page',1,'hidden'); ?>
-    	</div>
+    	
+    	<div class="grid panel-content">
+    	
+        	<div class="col sml-12 med-4">
+            	<?php
+                    echo plxToken::getTokenPostMethod();
+                    if($_SESSION['profil']<=PROFIL_MODERATOR) {
+                        echo '<input type="submit" name="delete" value="'.L_DELETE.'" onclick="Check=confirm(\''.L_ARTICLE_DELETE_CONFIRM.'\');if(Check==false) {return false;} else {this.form.target=\'_self\';return true;}" /> ';
+                    }
+                ?>
+                <?php plxUtils::printInput('page',1,'hidden'); ?>
+        	</div>
+        	<div id="pagination"  class="col sml-12 med-8 text-right">
+            	<?php
+            	# Hook Plugins
+            	eval($plxAdmin->plxPlugins->callHook('AdminIndexPagination'));
+            	# Affichage de la pagination
+            	if($arts) { # Si on a des articles (hors page)
+            		# Calcul des pages
+            		$last_page = ceil($nbArtPagination/$plxAdmin->bypage);
+            		$stop = $plxAdmin->page + 2;
+            		if($stop<5) $stop=5;
+            		if($stop>$last_page) $stop=$last_page;
+            		$start = $stop - 4;
+            		if($start<1) $start=1;
+            		# Génération des URLs
+            		$artTitle = (!empty($_GET['artTitle'])?'&amp;artTitle='.urlencode($_GET['artTitle']):'');
+            		$p_url = 'articles.php?page='.($plxAdmin->page-1).$artTitle;
+            		$n_url = 'articles.php?page='.($plxAdmin->page+1).$artTitle;
+            		$l_url = 'articles.php?page='.$last_page.$artTitle;
+            		$f_url = 'articles.php?page=1'.$artTitle;
+            		# Affichage des liens de pagination
+            		printf('<span class="p_page">'.L_PAGINATION.'</span>', '<input style="text-align:right;width:35px" onchange="window.location.href=\'articles.php?page=\'+this.value+\''.$artTitle.'\'" value="'.$plxAdmin->page.'" />', $last_page);
+            		$s = $plxAdmin->page>2 ? '<a href="'.$f_url.'" title="'.L_PAGINATION_FIRST_TITLE.'">&laquo;</a>' : '&laquo;';
+            		echo '<span class="p_first">'.$s.'</span>';
+            		$s = $plxAdmin->page>1 ? '<a href="'.$p_url.'" title="'.L_PAGINATION_PREVIOUS_TITLE.'">&lsaquo;</a>' : '&lsaquo;';
+            		echo '<span class="p_prev">'.$s.'</span>';
+            		for($i=$start;$i<=$stop;$i++) {
+            			$s = $i==$plxAdmin->page ? $i : '<a href="'.('articles.php?page='.$i.$artTitle).'" title="'.$i.'">'.$i.'</a>';
+            			echo '<span class="p_current">'.$s.'</span>';
+            		}
+            		$s = $plxAdmin->page<$last_page ? '<a href="'.$n_url.'" title="'.L_PAGINATION_NEXT_TITLE.'">&rsaquo;</a>' : '&rsaquo;';
+            		echo '<span class="p_next">'.$s.'</span>';
+            		$s = $plxAdmin->page<($last_page-1) ? '<a href="'.$l_url.'" title="'.L_PAGINATION_LAST_TITLE.'">&raquo;</a>' : '&raquo;';
+            		echo '<span class="p_last">'.$s.'</span>';
+            	}
+            	?>
+            </div>
+		</div>
     </div>
 
 </form>
 
-<p id="pagination">
-	<?php
-	# Hook Plugins
-	eval($plxAdmin->plxPlugins->callHook('AdminIndexPagination'));
-	# Affichage de la pagination
-	if($arts) { # Si on a des articles (hors page)
-		# Calcul des pages
-		$last_page = ceil($nbArtPagination/$plxAdmin->bypage);
-		$stop = $plxAdmin->page + 2;
-		if($stop<5) $stop=5;
-		if($stop>$last_page) $stop=$last_page;
-		$start = $stop - 4;
-		if($start<1) $start=1;
-		# Génération des URLs
-		$artTitle = (!empty($_GET['artTitle'])?'&amp;artTitle='.urlencode($_GET['artTitle']):'');
-		$p_url = 'articles.php?page='.($plxAdmin->page-1).$artTitle;
-		$n_url = 'articles.php?page='.($plxAdmin->page+1).$artTitle;
-		$l_url = 'articles.php?page='.$last_page.$artTitle;
-		$f_url = 'articles.php?page=1'.$artTitle;
-		# Affichage des liens de pagination
-		printf('<span class="p_page">'.L_PAGINATION.'</span>', '<input style="text-align:right;width:35px" onchange="window.location.href=\'articles.php?page=\'+this.value+\''.$artTitle.'\'" value="'.$plxAdmin->page.'" />', $last_page);
-		$s = $plxAdmin->page>2 ? '<a href="'.$f_url.'" title="'.L_PAGINATION_FIRST_TITLE.'">&laquo;</a>' : '&laquo;';
-		echo '<span class="p_first">'.$s.'</span>';
-		$s = $plxAdmin->page>1 ? '<a href="'.$p_url.'" title="'.L_PAGINATION_PREVIOUS_TITLE.'">&lsaquo;</a>' : '&lsaquo;';
-		echo '<span class="p_prev">'.$s.'</span>';
-		for($i=$start;$i<=$stop;$i++) {
-			$s = $i==$plxAdmin->page ? $i : '<a href="'.('articles.php?page='.$i.$artTitle).'" title="'.$i.'">'.$i.'</a>';
-			echo '<span class="p_current">'.$s.'</span>';
-		}
-		$s = $plxAdmin->page<$last_page ? '<a href="'.$n_url.'" title="'.L_PAGINATION_NEXT_TITLE.'">&rsaquo;</a>' : '&rsaquo;';
-		echo '<span class="p_next">'.$s.'</span>';
-		$s = $plxAdmin->page<($last_page-1) ? '<a href="'.$l_url.'" title="'.L_PAGINATION_LAST_TITLE.'">&raquo;</a>' : '&raquo;';
-		echo '<span class="p_last">'.$s.'</span>';
-	}
-	?>
-</p>
+
 
 <?php
 # Hook Plugins
