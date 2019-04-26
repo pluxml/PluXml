9485a96984c5b32fb4115b30cbe486f2ac708a2e
P3ter <contact@p3ter.fr>
2019-04-26 22:06:32 +0200

    Templating dashboard

--- core/admin/index.php
+++ core/admin/index.php
@@ -29,50 +29,42 @@ include __DIR__ .'/top.php';
 </div>
 
 <div class="grid">
-	<div class="col sml-12 med-6 panel">
-		<div class="grid">
-			<div class="col sml-12">
-				<div class="panel-content">
-            		<h3 class="no-margin">Mes brouillons</h3>
-            	</div>
+	<div class="col sml-12 med-6">
+		<div class="grid panel">
+			<div class="col sml-12 panel-content panel-title">
+           		<h3 class="no-margin">Mes brouillons</h3>
             </div>
-            <div class="col sml-12">
-            	<div class="panel-content">
-                    <?php
-            		# Récupération des articles
-                    $plxAdmin->prechauffage($plxAdmin->motif('draft','all'));
-                    $arts = $plxAdmin->getArticles('all');
+            <div class="col sml-12 panel-content">
+				<?php
+            	# Récupération des articles
+                $plxAdmin->prechauffage($plxAdmin->motif('draft','all'));
+                $arts = $plxAdmin->getArticles('all');
 
-                    # Liste des articles
-            		if($arts) {
-            			while($plxAdmin->plxRecord_arts->loop()) { # Pour chaque article
-            			    $idArt = $plxAdmin->plxRecord_arts->f('numero');
-            			    $author = plxUtils::getValue($plxAdmin->aUsers[$plxAdmin->plxRecord_arts->f('author')]['name']);
-            				echo plxDate::formatDate($plxAdmin->plxRecord_arts->f('date'));
-            				echo '<a href="article.php?a='.$idArt.'" title="'.L_ARTICLE_EDIT_TITLE.'">'.plxUtils::strCheck($plxAdmin->plxRecord_arts->f('title')).'</a>';
-            				echo plxUtils::strCheck($author);
-            				echo '<a href="article.php?a='.$idArt.'" title="'.L_ARTICLE_EDIT_TITLE.'">'.L_ARTICLE_EDIT.'</a>';
-            			}
-            		} else { # Aucun article dans la liste
-            			echo L_NO_ARTICLE;
-            		}
-            		?>
-    			</div>
+                # Liste des articles
+            	if($arts) {
+            	   while($plxAdmin->plxRecord_arts->loop()) { # Pour chaque article
+                	   $idArt = $plxAdmin->plxRecord_arts->f('numero');
+                       $author = plxUtils::getValue($plxAdmin->aUsers[$plxAdmin->plxRecord_arts->f('author')]['name']);
+                	   echo plxDate::formatDate($plxAdmin->plxRecord_arts->f('date'));
+                	   echo '<a href="article.php?a='.$idArt.'" title="'.L_ARTICLE_EDIT_TITLE.'">'.plxUtils::strCheck($plxAdmin->plxRecord_arts->f('title')).'</a>';
+                	   echo plxUtils::strCheck($author);
+                	   echo '<a href="article.php?a='.$idArt.'" title="'.L_ARTICLE_EDIT_TITLE.'">'.L_ARTICLE_EDIT.'</a>';
+                   }
+            	} else { # Aucun article dans la liste
+            		  echo L_NO_ARTICLE;
+            	}
+            	?>
     		</div>
 		</div>
 	</div>
-	<div class="col sml-12 med-6 panel">
-		<div class="grid">
-			<div class="col sml-12">
-				<div class="panel-content">
-            		<h3 class="no-margin">Actualités</h3>
-            	</div>
+	<div class="col sml-12 med-6">
+		<div class="grid panel">
+			<div class="col sml-12 panel-content panel-title">
+           		<h3 class="no-margin">Actualités</h3>
             </div>
-    		<div class="col sml-12">
-    			<div class="panel-content">
-            		<p>Blog</p>
-            		<p>Forum</p>
-            	</div>
+    		<div class="col sml-12 panel-content">
+           		<p>Blog</p>
+           		<p>Forum</p>
             </div>
     	</div>
 	</div>
@@ -83,15 +75,12 @@ include __DIR__ .'/top.php';
 if($_SESSION['profil'] <= PROFIL_MODERATOR) {
     echo'
         <div class="grid">
-            <div class="col sml-12 panel">
-                <div class="grid">
-			        <div class="col sml-12">
-                        <div class="panel-content">
-				            <h3 class="no-margin">Articles en modération</h3>
-                        </div>
+            <div class="col sml-12">
+                <div class="grid panel">
+			        <div class="col sml-12 panel-content panel-title">
+			            <h3 class="no-margin">Articles en modération</h3>
                     </div>
-                    <div class="col sml-12">
-                        <div class="panel-content">
+                    <div class="col sml-12 panel-content">
 	';
 	# Récupération des articles
     $plxAdmin->prechauffage($plxAdmin->motif('mod','all', $userId));
@@ -111,7 +100,6 @@ if($_SESSION['profil'] <= PROFIL_MODERATOR) {
 		echo L_NO_ARTICLE;
 	}
 	echo'
-                       </div>
                    </div>
         	   </div>
             </div>
@@ -125,17 +113,13 @@ if($_SESSION['profil'] <= PROFIL_MODERATOR) {
 if($_SESSION['profil'] <= PROFIL_MODERATOR) {
     echo'
         <div class="grid">
-            <div class="col sml-12 panel">
-                <div class="grid">
-			        <div class="col sml-12">
-                        <div class="panel-content">
-				            <h3 class="no-margin">Commenaires en modération</h3>
-                        </div>
+            <div class="col sml-12">
+                <div class="grid panel">
+			        <div class="col sml-12 panel-content panel-title">
+			            <h3 class="no-margin">Commenaires en modération</h3>
                     </div>
-                    <div class="col sml-12">
-                        <div class="panel-content">
-                            <p>Liste des commantaires</p>
-                       </div>
+                    <div class="col sml-12 panel-content">
+                        <p>Liste des commantaires</p>
                    </div>
         	   </div>
             </div>
