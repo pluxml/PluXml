8335a7f8963b893515e299ece852013fd0000b24
Pedro CADETE <pcadete@conforama.fr>
2019-07-01 17:53:29 +0200

    Revert "fixed sidebar and header (desktop)"
    This reverts commit 976b7c4ccbb178ccfd64746c195ff57c34887b23.

--- core/admin/views/masterTemplate.php
+++ core/admin/views/masterTemplate.php
@@ -23,52 +23,49 @@
 
 <body id="<?= $scriptName ?>">
 
-	<div class="flex-container-column">
-		<header id="header" role="banner" class="autogrid pas header">
-    		<div>
-    			<a href="<?= $adminUrl ?>"><h1 class="h3-like"><strong><?= $siteTitle ?></strong></h1></a>
-    		</div>
-        	<div class="txtright">
-               	<ul class="unstyled">
-               		<li class="inbl">
-                		<a class="" href="<?= $siteUrl ?>" title="<?= $siteTitle ?>"><small><?= $backToSite ?></small></a>
-                	</li>
-    				<?= $blogLink ?>
-            		<li class="inbl">
-            			<a class="logout" href="<?= $logOutUrl ?>" title="<?= $logOutTitle ?>"><small><?= $logOut ?></small></a>
-            		</li>
-            		<li class="inbl">
-            			<span class="badge"><img class="profil" src="<?= $profilPicture ?>"></span>
-    				</li>
-            	</ul>
-            </div>
-    	</header>
-    	
-		<div class="flex-container">
-    
-            <aside class="mod w250p pam aside">
-            	<nav id="navigation" role="navigation">
-            		<ul id="responsive-menu" class="menu vertical expanded">
-            			<?= $menuContent ?>
-            		</ul>
-            		<ul class="unstyled">
-               			<li>
-               				<small><a class="version" title="PluXml" href="http://www.pluxml.org">PluXml <?= $pluxmlVersion ?></a></small>
-               				<?= $pluxmlMaj ?>
-            			</li>
-            		</ul>
-            	</nav>
-            </aside>
-    
-        	<main id="main" role="main" class="flex-item-fluid pam main">
-    			<?= $adminMessage ?>
-        		<?= $hookAdminTopBottom ?>
-    
-        		<?= $mainContent ?>
+	<header id="header" role="banner" class="autogrid pas header">
+		<div>
+			<a href="<?= $adminUrl ?>"><h1 class="h3-like"><strong><?= $siteTitle ?></strong></h1></a>
+		</div>
+    	<div class="txtright">
+           	<ul class="unstyled">
+           		<li class="inbl">
+            		<a class="" href="<?= $siteUrl ?>" title="<?= $siteTitle ?>"><small><?= $backToSite ?></small></a>
+            	</li>
+				<?= $blogLink ?>
+        		<li class="inbl">
+        			<a class="logout" href="<?= $logOutUrl ?>" title="<?= $logOutTitle ?>"><small><?= $logOut ?></small></a>
+        		</li>
+        		<li class="inbl">
+        			<span class="badge"><img class="profil" src="<?= $profilPicture ?>"></span>
+				</li>
+        	</ul>
+        </div>
+	</header>
 
-    		</main>
+    <div class="flex-container">
+    
+        <aside class="mod w250p pam aside">
+        	<nav id="navigation" role="navigation">
+        		<ul id="responsive-menu" class="menu vertical expanded">
+        			<?= $menuContent ?>
+        		</ul>
+        		<ul class="unstyled">
+           			<li>
+           				<small><a class="version" title="PluXml" href="http://www.pluxml.org">PluXml <?= $pluxmlVersion ?></a></small>
+           				<?= $pluxmlMaj ?>
+        			</li>
+        		</ul>
+        	</nav>
+        </aside>
 
-    	</div>
+    	<main id="main" role="main" class="flex-item-fluid pam">
+			<?= $adminMessage ?>
+    		<?= $hookAdminTopBottom ?>
+
+    		<?= $mainContent ?>
+
+		</main>
 
 	</div>
 
