a345783f2b9d40c82fe4883f33f3c228f71172d2
P3ter <contact@p3ter.fr>
2019-12-06 21:33:29 +0100

    [default theme] header responsive fixes

--- themes/defaut/header.php
+++ themes/defaut/header.php
@@ -45,7 +45,7 @@
 					<nav class="nav">
 
 						<div class="responsive-menu">
-							<label for="menu"><span class="float-left"><?php $plxShow->lang('MENU') ?></span></label>
+							<label for="menu"></label>
 							<input type="checkbox" id="menu">
 							<ul class="menu">
 								<?php $plxShow->staticList($plxShow->getLang('HOME'),'<li class="#static_class #static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
