f6363b10c74a81824cdbc0019fee37c2a9470d3c
bazooka07 <jpourrez@gmail.com>
2020-01-07 13:09:22 +0100

    Fix dossier des templates

--- update/index.php
+++ update/index.php
@@ -1,10 +1,10 @@
 <?php
-define('PLX_ROOT', '../');
-define('PLX_CORE', PLX_ROOT.'core/');
+const PLX_ROOT = '../';
+const PLX_CORE = PLX_ROOT . 'core/';
 include(PLX_ROOT.'config.php');
 include(PLX_CORE.'lib/config.php');
 
-define('PLX_UPDATER', true);
+const PLX_UPDATER = true;
 
 # On verifie que PluXml est installé
 if(!file_exists(path('XMLFILE_PARAMETERS'))) {
