5ffa409d6a2bf1ce2d8c3e4d8ecec82d21225b4b
P3ter <contact@p3ter.fr>
2020-03-26 19:58:03 +0100

    [plx6] medias mode grid WIP

--- core/admin/tags/mediasGridTag.php
+++ core/admin/tags/mediasGridTag.php
@@ -3,10 +3,19 @@
  * Medias list mode tag
  * @author	Pedro "P3ter" CADETE"
  **/
-
-use Pluxml\PlxDate;
-use Pluxml\PlxUtils;
-
 ?>
 
-GRID
+<?php
+	if($plxMedias->aFiles) {
+		foreach($plxMedias->aFiles as $v) { # Pour chaque fichier
+			$isImage = in_array(strtolower($v['extension']), $plxMedias->img_supported);
+			$title = pathinfo($v['name'], PATHINFO_FILENAME);
+			if(is_file($v['path']) AND $isImage) {
+				echo '<a class="overlay" title="'.$title.'" href="'.$v['path'].'"><img alt="'.$title.'" src="'.$v['.thumb'].'" class="thumb" /></a>';
+			}
+			else
+				echo '<img alt="" src="'.$v['.thumb'].'" class="thumb" />';
+		}
+	}
+	else echo L_MEDIAS_NO_FILE;
+?>
