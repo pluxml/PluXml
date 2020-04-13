e407b16a237fe33517ea48fc32afd329f0483464
Mindiell <github@mindiell.net>
2020-04-13 14:17:53 +0200

    install & config fixes

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
-const PLX_CONFIG_PATH = 'data/configuration/';
+const PLX_DATA_PATH = PLX_ROOT.'data/';
+const PLX_CONFIG_PATH = PLX_DATA_PATH.'configuration/';
+
+const PLX_PLUGINS_PATH = PLX_ROOT.'plugins/';
+const PLX_THEMES_PATH = PLX_ROOT.'themes/';
 ?>
