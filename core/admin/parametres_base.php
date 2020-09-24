4026f41f65609a03f8ea5f889d5e79d8363d0e07
Pedro CADETE <pcadete@conforama.fr>
2020-09-24 17:13:48 +0200

    fix paramtres_base

--- core/admin/parametres_base.php
+++ core/admin/parametres_base.php
@@ -39,83 +39,65 @@ include __DIR__ . '/top.php';
     <?php eval($plxAdmin->plxPlugins->callHook('AdminSettingsBaseTop')) # Hook Plugins ?>
 
     <fieldset class="config">
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+        <div class="grid-2">
+            <div>
                 <label for="id_title"><?php echo L_CONFIG_BASE_SITE_TITLE ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printInput('title', plxUtils::strCheck($plxAdmin->aConf['title'])); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_description"><?php echo L_CONFIG_BASE_SITE_SLOGAN ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printInput('description', plxUtils::strCheck($plxAdmin->aConf['description'])); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_meta_description"><?php echo L_CONFIG_META_DESCRIPTION ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printInput('meta_description', plxUtils::strCheck($plxAdmin->aConf['meta_description'])); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_meta_keywords"><?php echo L_CONFIG_META_KEYWORDS ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printInput('meta_keywords', plxUtils::strCheck($plxAdmin->aConf['meta_keywords'])); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_default_lang"><?php echo L_CONFIG_BASE_DEFAULT_LANG ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('default_lang', plxUtils::getLangs(), $plxAdmin->aConf['default_lang']) ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_timezone"><?php echo L_TIMEZONE ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('timezone', plxTimezones::timezones(), $plxAdmin->aConf['timezone']); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_allow_com"><?php echo L_ALLOW_COMMENTS ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('allow_com', array('1' => L_YES, '0' => L_NO), $plxAdmin->aConf['allow_com']); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_mod_com"><?php echo L_CONFIG_BASE_MODERATE_COMMENTS ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('mod_com', array('1' => L_YES, '0' => L_NO), $plxAdmin->aConf['mod_com']); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_mod_art"><?php echo L_CONFIG_BASE_MODERATE_ARTICLES ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('mod_art', array('1' => L_YES, '0' => L_NO), $plxAdmin->aConf['mod_art']); ?>
             </div>
-        </div>
-        <div class="grid">
-            <div class="col sml-12 med-5 label-centered">
+            <div>
                 <label for="id_enable_rss"><?php echo L_CONFIG_BASE_ENABLE_RSS ?>&nbsp;:</label>
             </div>
-            <div class="col sml-12 med-7">
+            <div>
                 <?php plxUtils::printSelect('enable_rss', array('1' => L_YES, '0' => L_NO), $plxAdmin->aConf['enable_rss']); ?>
             </div>
         </div>
