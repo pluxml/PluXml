5dd8d48318817775423dac7b1c752fb0749f3459
Jean-Pierre Pourrez <46094001+kazimentou@users.noreply.github.com>
2020-11-10 21:09:10 +0100

    Fix typo core/lang/en/admin.php (#499)
    Co-authored-by: bazooka07 <jpourrez@gmail.com>

--- core/lang/en/admin.php
+++ core/lang/en/admin.php
@@ -529,5 +529,5 @@ const L_THEMES_TITLE = 'Manage themes';
 const L_HELP = 'Help';
 const L_HELP_TITLE = 'See help';
 const L_BACK_TO_THEMES = 'Back to themes';
-const L_CONFIG_THEME_UPDATE = 'Select this theme'
+const L_CONFIG_THEME_UPDATE = 'Select this theme';
 ?>
