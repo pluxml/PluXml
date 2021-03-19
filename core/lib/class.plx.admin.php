53d41f17099088daf9c58f57d9a1484a274ade3d
bazooka07 <jpourrez@gmail.com>
2021-03-19 09:08:23 +0100

    Fix use of uasort() for plxAdmin

--- core/lib/class.plx.admin.php
+++ core/lib/class.plx.admin.php
@@ -645,7 +645,7 @@ RewriteRule ^feed\/(.*)$ feed.php?$1 [L]
 				}
 			}
 			# On va trier les clés selon l'ordre choisi
-			if(sizeof($this->aCats)>0) uasort($this->aCats, function($a, $b){return $a["ordre"]>$b["ordre"];});
+			if(sizeof($this->aCats) > 1) uasort($this->aCats, function($a, $b) { return intval($a['ordre']) - intval($b['ordre']); } );
 		}
 		# sauvegarde
 		if($action) {
@@ -777,8 +777,9 @@ RewriteRule ^feed\/(.*)$ feed.php?$1 [L]
 				}
 			}
 			# On va trier les clés selon l'ordre choisi
-			if(sizeof($this->aStats)>0)
-				uasort($this->aStats, function($a, $b){return $a["ordre"]>$b["ordre"];});
+			if(sizeof($this->aStats) > 1) {
+				uasort($this->aStats, function($a, $b) { return intval($a['ordre']) - intval($b['ordre']); } );
+			}
 		}
 		# sauvegarde
 		if($action) {
