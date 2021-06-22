425074681ba6998648423f182c18683dd93b50ce
Pedro CADETE <contact@p3ter.fr>
2021-06-22 16:31:31 +0200

    update index.php templating

--- update/index.php
+++ update/index.php
@@ -5,7 +5,7 @@ include PLX_ROOT . 'core/lib/config.php';
 
 const PLX_UPDATER = true;
 
-# Chargement des langues
+// Languages loading
 $lang = (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) : DEFAULT_LANG;
 if (isset($_POST['default_lang'])) $lang = $_POST['default_lang'];
 if (!array_key_exists($lang, plxUtils::getLangs())) {
@@ -15,26 +15,31 @@ loadLang(PLX_CORE . 'lang/' . $lang . '/core.php');
 loadLang(PLX_CORE . 'lang/' . $lang . '/admin.php');
 loadLang(PLX_CORE . 'lang/' . $lang . '/update.php');
 
-# On vérifie que PHP 5 ou superieur soit installé
+// PHP version check
 if (version_compare(PHP_VERSION, PHP_VERSION_MIN, '<')) {
     header('Content-Type: text/plain charset=UTF-8');
     printf(L_WRONG_PHP_VERSION, PHP_VERSION_MIN);
     exit;
 }
 
-# Echappement des caractères
+// Remove slash characters
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_POST = plxUtils::unSlash($_POST);
 }
 
-# Création de l'objet principal et lancement du traitement
-$plxUpdater = new plxUpdater();
-
+// Start PHP session
 plxUtils::cleanHeaders();
 session_set_cookie_params(0, "/", $_SERVER['SERVER_NAME'], isset($_SERVER["HTTPS"]), true);
 session_start();
-# Control du token du formulaire
+
+// CSRF token validation
 plxToken::validateFormToken($_POST);
+
+// Data
+$plxUpdater = new plxUpdater();
+$root = PLX_ROOT . dirname(PLX_CONFIG_PATH);
+$writable = is_writable($root);
+$version = isset($_POST['version']) ? $_POST['version'] : $plxUpdater->oldVersion;
 ?>
 <!DOCTYPE html>
 <head>
@@ -60,73 +65,54 @@ plxToken::validateFormToken($_POST);
             <h1><?= L_UPDATE_TITLE . ' ' . plxUtils::strCheck($plxUpdater->newVersion) ?></h1>
         </header>
 
-        <?php
-        $root = PLX_ROOT . dirname(PLX_CONFIG_PATH);
-        $writable = is_writable($root);
-        if (!$writable) {
-            ?>
-            <p class="alert--danger"><?php printf(L_WRITE_NOT_ACCESS, $root) ?></p>
-            <?php
-        }
-
-        if (!$writable or empty($_POST['submit'])) {
-            if (version_compare($plxUpdater->oldVersion, $plxUpdater->newVersion) >= 0) {
-                ?>
-                <p><strong><?= L_UPDATE_UPTODATE ?></strong></p>
-                <p><?= L_UPDATE_NOT_AVAILABLE ?></p>
-                <p><a href="<?= PLX_ROOT; ?>index.php" title="<?= L_UPDATE_BACK ?>"><?= L_UPDATE_BACK ?></a></p>
-                <?php
-            } else {
-                ?>
-
+        <?php if (!$writable or empty($_POST['submit'])): ?>
+            <?php if (version_compare($plxUpdater->oldVersion, $plxUpdater->newVersion) >= 0): ?>
                 <div class="txtcenter">
-                    <form method="post">
-                        <fieldset class=pln">
-                            <div class="inbl">
-                                <label for="id_default_lang"><?php echo L_SELECT_LANG ?>&nbsp;:</label>
-                            </div>
-                            <div class="inbl">
-                                <?php plxUtils::printSelect('default_lang', plxUtils::getLangs(), $lang) ?>&nbsp;
-                                <input class="btn--inverse" type="submit" name="select_lang"
-                                       role="button" value="<?php echo L_INPUT_CHANGE ?>"/>
-                                <?php echo plxToken::getTokenPostMethod() ?>
-                            </div>
-                        </fieldset>
-                    </form>
+                    <p><strong><?= L_UPDATE_UPTODATE ?></strong></p>
+                    <p><?= L_UPDATE_NOT_AVAILABLE ?></p>
+                    <p><a href="<?= PLX_ROOT; ?>index.php" title="<?= L_UPDATE_BACK ?>"><?= L_UPDATE_BACK ?></a></p>
                 </div>
-
-                <form method="post">
+            <?php else: ?>
+                <form method="post" class="txtcenter">
+                    <fieldset class=pln">
+                        <div class="inbl">
+                            <label for="id_default_lang"><?= L_SELECT_LANG ?></label>
+                        </div>
+                        <div class="inbl">
+                            <?php plxUtils::printSelect('default_lang', plxUtils::getLangs(), $lang) ?>&nbsp;
+                            <input class="btn--inverse" type="submit" name="select_lang"
+                                   role="button" value="<?= L_INPUT_CHANGE ?>"/>
+                            <?= plxToken::getTokenPostMethod() ?>
+                        </div>
+                    </fieldset>
+                </form>
+                <?php if (!$writable): ?>
+                    <p class="alert--danger"><?php printf(L_WRITE_NOT_ACCESS, $root) ?></p>
+                <?php endif; ?>
+                <form method="post" class="txtcenter">
                     <fieldset>
                         <p><strong><?= L_UPDATE_WARNING1 . ' ' . $plxUpdater->oldVersion ?></strong></p>
-                        <?php
-                        if (empty($plxUpdater->oldVersion)) {
-                            ?>
+                        <?php if (empty($plxUpdater->oldVersion)): ?>
                             <p><?= L_UPDATE_SELECT_VERSION ?></p>
                             <p><?php plxUtils::printSelect('version', plxUpdater::VERSIONS); ?></p>
                             <p><?= L_UPDATE_WARNING2 ?></p>
-                            <?php
-                        }
-                        ?>
+                        <?php endif; ?>
                         <p><?php printf(L_UPDATE_WARNING3, preg_replace('@^([^/]+).*@', '$1', $plxUpdater->plxAdmin->aConf['racine_articles'])); ?></p>
-                        <p class="mtm"><input type="submit" name="submit" role="button" value="<?= L_UPDATE_START ?>" <?php if (!$writable) {
-                                echo 'disabled';
-                            } ?> /></p>
-                        <?php echo plxToken::getTokenPostMethod() ?>
+                        <p class="mtm"><input type="submit" name="submit" role="button"
+                                              value="<?= L_UPDATE_START ?>"
+                                              <?php if (!$writable): ?>disabled<?php endif; ?> /></p>
+                        <?= plxToken::getTokenPostMethod() ?>
                     </fieldset>
                 </form>
-                <?php
-            }
-        } else {
-            $version = isset($_POST['version']) ? $_POST['version'] : $plxUpdater->oldVersion;
-            $plxUpdater->startUpdate($version);
-            ?>
-            <form action="<?= PLX_ROOT ?>">
-                <input type="submit" role="button" value="<?= L_UPDATE_BACK ?>"/>
-            </form>
-            <?php
-        }
-        ?>
+            <?php endif; ?>
+        <?php else: ?>
+            <div class="txtcenter">
+                <?php $plxUpdater->startUpdate($version); ?>
+                <form action="<?= PLX_ROOT ?>">
+                    <input type="submit" role="button" value="<?= L_UPDATE_BACK ?>"/>
+                </form>
+            </div>
+        <?php endif; ?>
     </section>
 </main>
 </body>
-</html>
