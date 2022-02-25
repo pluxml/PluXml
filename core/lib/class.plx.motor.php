ae319901b9fb8986313c99ac3bf2af5f0581aa59
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-02-25 17:14:35 +0100

    Update class.plx.motor.php
    modification de la modif illogique initial :

--- core/lib/class.plx.motor.php
+++ core/lib/class.plx.motor.php
@@ -682,7 +682,7 @@ class plxMotor {
 					return in_array($item, $ids);
 				}
 			);
-			if(count($artCats) == 1 and key($artCats) == 'draft') {
+			if(count($artCats) == 1 and array_values($artCats)[0] == 'draft') {
 				$artCats[] = '000';
 			}
 			return array(
