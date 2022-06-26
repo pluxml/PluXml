1db2aa955e8616b22c8b3a7eb3c2672e6b7890c0
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2022-06-26 10:20:06 +0200

    Update class.plx.motor.php
    # configuration du nombre d'articles affichés en **home**  applicable aussi au mode **categorie** (au lieu de 5 par défaut) // ligne 192

--- core/lib/class.plx.motor.php
+++ core/lib/class.plx.motor.php
@@ -189,7 +189,7 @@ class plxMotor {
 				$this->motif = '#^\d{4}.((?:\d|home|,)*(?:'.$this->cible.')(?:\d|home|,)*).\d{3}.\d{12}.[\w-]+.xml$#'; # Motif de recherche
 				$this->template = $this->aCats[$this->cible]['template'];
 				$this->tri = $this->aCats[$this->cible]['tri']; # Recuperation du tri des articles
-				$this->bypage = $this->aCats[$this->cible]['bypage'] > 0 ? $this->aCats[$this->cible]['bypage'] : $this->bypage;
+				$this->bypage = !empty($this->aCats[$this->cible]['bypage']) ? $this->aConf['bypage'] : $this->bypage;
 			}
 			elseif(isset($this->aCats[$this->cible])) { # Redirection 301
 				if($this->aCats[$this->cible]['url']!=$capture[2]) {
