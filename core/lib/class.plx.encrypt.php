83793acf2598610f2fa4b3bf14ddb4d8a9f11be2
Jean-Pierre Pourrez <kazimentou@free.fr>
2023-01-24 15:04:28 +0100

    refacto plxEncrypt

--- core/lib/class.plx.encrypt.php
+++ core/lib/class.plx.encrypt.php
@@ -3,52 +3,85 @@
  * Classe plxEncrypt responsable du cryptage et décryptage de données
  *
  * @package PLX
- * @author	Stephane F
+ * @author	Stephane F, Jean-Pierre Pourrez "bazooka07"
  **/
 
-const ENCRYPTION_KEY = 'ab12cd34#$';
+# https://github.com/abcarroll/hack3r3d-php-openssl-cryptor/blob/master/src/Cryptor.php
 
 class plxEncrypt {
+	const FORMAT_B64 = true; # either HEX
 
-	/**
-	 * Méthode qui crypte le texte $plainText
-	 *
-	 * @param	plainText	chaîne à crypter
-	 * @return	string	chaîne cryptée
-	 * @author	Stephane F.
-	 **/
-	private static function base64url_encode($plainText) {
+	private static function _key() {
+		return
+		  dechex(filemtime(__DIR__ . '/class.plx.motor.php') & 0x7fffff)
+		. dechex(filemtime(__DIR__ . '/class.plx.admin.php') & 0x7fffff)
+		. dechex(filemtime(__DIR__ . '/class.plx.show.php') & 0x7fffff);
 
-		$base64 = base64_encode($plainText);
-		$base64url = strtr($base64, '+/=', '-_,');
-		return $base64url;
 	}
 
-	/**
-	 * Méthode qui décrypte le texte $plainText
-	 *
-	 * @param	plainText	chaîne à décrypter
-	 * @return	string	chaîne décryptée
-	 * @author	Stephane F.
-	 **/
-	private static function base64url_decode($plainText) {
+	private static function _getCipherMethod() {
+		$methods = openssl_get_cipher_methods(true);
+		foreach(['aes-256-ctr', 'aes-192-ctr', 'aes-128-ctr', 'aes256',] as $f) {
+			if(in_array($f, $methods)) {
+				return $f;
+			}
+		}
 
-		$base64url = strtr($plainText, '-_,', '+/=');
-		$base64 = base64_decode($base64url);
-		return $base64;
+		return false;
 	}
 
-	public static function encryptId($int, $class='') {
+	public static function encryptId($str, $class='') {
+		$cipher_algo = self::_getCipherMethod();
+		if($cipher_algo) {
+			$iv_num_bytes = openssl_cipher_iv_length($cipher_algo);
+			$iv = openssl_random_pseudo_bytes($iv_num_bytes);
+			$hash_key = openssl_digest(self::_key(), 'sha256', true);
+			$encrypt = openssl_encrypt(
+				$str,
+				$cipher_algo,
+				$hash_key,
+				OPENSSL_RAW_DATA,
+				$iv
+			);
 
-		return plxEncrypt::base64url_encode($int.'*'.substr(sha1($class.$int.ENCRYPTION_KEY), 0, 6));
+			if(self::FORMAT_B64) {
+				return base64_encode($iv . $encrypt);
+			} else {
+				$parts = unpack('H*', $iv . $encrypt);
+				if(isset($parts[1])) {
+					return $parts[1];
+				}
+			}
+		}
+
+		# No encryption
+		return $str;
 	}
 
-	public static function decryptId($int, $class='') {
+	public static function decryptId($str, $class='') {
+		$cipher_algo = self::_getCipherMethod();
+		if($cipher_algo) {
+			$iv_num_bytes = openssl_cipher_iv_length($cipher_algo);
+			$raw = self::FORMAT_B64 ? base64_decode($str) : pack('H*', $str);
+			if(strlen($raw) > $iv_num_bytes) {
+				$iv = substr($raw, 0, $iv_num_bytes);
+				$raw = substr($raw, $iv_num_bytes);
+				$hash_key = openssl_digest(self::_key(), 'sha256', true);
+				return openssl_decrypt(
+					$raw,
+					$cipher_algo,
+					$hash_key, # key for encryption
+					OPENSSL_RAW_DATA,
+					$iv
+				);
+			}
 
-		$parts = explode('*', plxEncrypt::base64url_decode($int));
-		if(sizeof($parts) != 2)
-			return 0;
-		return substr(sha1($class.$parts[0].ENCRYPTION_KEY), 0, 6) === $parts[1] ? $parts[0] : 0;
+			return false;
+		}
+
+
+		# No encryption
+		return $str;
 	}
 
 }
