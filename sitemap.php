de71ad6dc1f88b8f831db88d595cc78fc2d90ea6
Jean-Pierre Pourrez <46094001+kazimentou@users.noreply.github.com>
2023-11-30 21:55:27 +0100

    Refacto sitemap.php (#676)
    Co-authored-by: Jean-Pierre Pourrez <kazimentou@free.fr>

--- sitemap.php
+++ sitemap.php
@@ -25,50 +25,55 @@ if(eval($plxMotor->plxPlugins->callHook('SitemapBegin'))) return;
 # chargement du fichier de langue
 loadLang(PLX_CORE . 'lang/' . $lang . '/core.php');
 
+$plxMotor->prechauffage();
+$plxMotor->demarrage();
+
 # On démarre la bufferisation
 ob_start();
 ob_implicit_flush(0);
 
-$plxMotor->prechauffage();
-$plxMotor->demarrage();
-
 # Entête XML
-echo '<?xml version="1.0" encoding="'.strtolower(PLX_CHARSET).'" ?>'."\n";
+echo XML_HEADER;
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc><?php echo $plxMotor->urlRewrite() ?></loc>
+		<loc><?= $plxMotor->urlRewrite() ?></loc>
 		<changefreq>weekly</changefreq>
 		<priority>1.0</priority>
 	</url>
 <?php
+
 # Les pages statiques
 foreach($plxMotor->aStats as $stat_num => $stat_info) {
-	if($stat_info['active']==1 AND $stat_num!=$plxMotor->aConf['homestatic']) {
-		echo "\n";
-		echo "\t<url>\n";
-        echo "\t\t<loc>".$plxMotor->racine."index.php?static".intval($stat_num)."/".$stat_info['url']."</loc>\n";
-		echo "\t\t<lastmod>".plxDate::formatDate($plxMotor->aStats[$stat_num]['date_update'],'#num_year(4)-#num_month-#num_day')."</lastmod>\n";
-		echo "\t\t<changefreq>monthly</changefreq>\n";
-		echo "\t\t<priority>0.8</priority>\n";
-		echo "\t</url>\n";
+	if($stat_info['active'] == 1 AND $stat_num != $plxMotor->aConf['homestatic']) {
+?>
+	<url>
+		<loc><?= $plxMotor->racine ?>index.php?static<?= intval($stat_num) . '/' . $stat_info['url'] ?></loc>
+		<lastmod><?= plxDate::formatDate($plxMotor->aStats[$stat_num]['date_update'],'#num_year(4)-#num_month-#num_day') ?></lastmod>
+		<changefreq>monthly</changefreq>
+		<priority>0.8</priority>
+	</url>
+<?php
 	}
 }
-eval($plxMotor->plxPlugins->callHook('SitemapStatics')); # Hook Plugins
+eval($plxMotor->plxPlugins->callHook('SitemapStatics')); # Hook plugins
+
 # Les catégories
 foreach($plxMotor->aCats as $cat_num => $cat_info) {
-	if($cat_info['active']==1 AND $cat_info['menu']=='oui' AND ($cat_info['articles']!=0 OR $plxMotor->aConf['display_empty_cat'])) {
-		echo "\n";
-		echo "\t<url>\n";
-        echo "\t\t<loc>".$plxMotor->racine."index.php?categorie".intval($cat_num)."/".$cat_info['url']."</loc>\n";
-		echo "\t\t<changefreq>weekly</changefreq>\n";
-		echo "\t\t<priority>0.8</priority>\n";
-		echo "\t</url>\n";
+	if($cat_info['active'] == 1 AND $cat_info['menu'] == 'oui' AND ($cat_info['articles'] != 0 OR $plxMotor->aConf['display_empty_cat'])) {
+?>
+	<url>
+		<loc><?= $plxMotor->racine ?>index.php?categorie <?= intval($cat_num) . '/' . $cat_info['url'] ?></loc>
+		<changefreq>weekly</changefreq>
+		<priority>0.8</priority>
+	</url>
+<?php
 	}
 }
 eval($plxMotor->plxPlugins->callHook('SitemapCategories')); # Hook Plugins
+
 # Les articles
-if($aFiles = $plxMotor->plxGlob_arts->query('/^[0-9]{4}.(?:[0-9]|home|,)*(?:'.$plxMotor->activeCats.'|home)(?:[0-9]|home|,)*.[0-9]{3}.[0-9]{12}.[a-z0-9-]+.xml$/','art','rsort', 0, false, 'before')) {
+if($aFiles = $plxMotor->plxGlob_arts->query('/^[0-9]{4}.(?:[0-9]|home|,)*(?:' . $plxMotor->activeCats . '|home)(?:[0-9]|home|,)*.[0-9]{3}.[0-9]{12}.[a-z0-9-]+.xml$/','art','rsort', 0, false, 'before')) {
 	$plxRecord_arts = false;
 	$array=array();
 	foreach($aFiles as $k=>$v) { # On parcourt tous les fichiers
@@ -80,17 +85,19 @@ if($aFiles = $plxMotor->plxGlob_arts->query('/^[0-9]{4}.(?:[0-9]|home|,)*(?:'.$p
 		# On boucle sur nos articles
 		while($plxRecord_arts->loop()) {
 			$num = intval($plxRecord_arts->f('numero'));
-			echo "\n";
-			echo "\t<url>\n";
-            echo "\t\t<loc>".$plxMotor->racine."index.php?article".$num."/".plxUtils::strCheck($plxRecord_arts->f('url'))."</loc>\n";
-			echo "\t\t<lastmod>".plxDate::formatDate($plxRecord_arts->f('date_update'),'#num_year(4)-#num_month-#num_day')."</lastmod>\n";
-			echo "\t\t<changefreq>monthly</changefreq>\n";
-			echo "\t\t<priority>0.5</priority>\n";
-			echo "\t</url>\n";
+?>
+	<url>
+		<loc><?= $plxMotor->racine ?>index.php?article<?= $num . '/' . plxUtils::strCheck($plxRecord_arts->f('url')) ?></loc>
+		<lastmod><?= plxDate::formatDate($plxRecord_arts->f('date_update'), '#num_year(4)-#num_month-#num_day') ?></lastmod>
+		<changefreq>monthly</changefreq>
+		<priority>0.5</priority>
+	</url>
+<?php
 		}
 	}
 }
 eval($plxMotor->plxPlugins->callHook('SitemapArticles')); # Hook Plugins
+
 ?>
 </urlset>
 <?php
@@ -101,6 +108,7 @@ $output = ob_get_clean();
 eval($plxMotor->plxPlugins->callHook('SitemapEnd')); # Hook Plugins
 
 # Restitution écran
+header('Content-Type: application/rss+xml');
+header('Content-Disposition: attachment; filename="sitemap.xml"');
+header('Content-Length: ' . strlen($output));
 echo $output;
-exit;
-?>
