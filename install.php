91d86e93956c8f02e222416f7cdb31117e5954b6
Jean-Pierre Pourrez <46094001+kazimentou@users.noreply.github.com>
2024-01-12 18:13:37 +0100

    miscallenous fixes (#701)
    Co-authored-by: Jean-Pierre Pourrez <kazimentou@free.fr>

--- install.php
+++ install.php
@@ -42,14 +42,14 @@ loadLang(PLX_CORE.'lang/'.$lang.'/core.php');
 
 # On vérifie la version minimale de PHP
 if(version_compare(PHP_VERSION, PHP_VERSION_MIN, '<')){
-	header('Content-Type: text/plain charset=UTF-8');
+	header('Content-Type: text/plain; charset=UTF-8');
 	echo L_WRONG_PHP_VERSION;
 	exit;
 }
 
 # On vérifie que PluXml n'est pas déjà installé
 if(file_exists(path('XMLFILE_PARAMETERS'))) {
-	header('Content-Type: text/plain charset=UTF-8');
+	header('Content-Type: text/plain; charset=UTF-8');
 	echo L_ERR_PLUXML_ALREADY_INSTALLED;
 	exit;
 }
