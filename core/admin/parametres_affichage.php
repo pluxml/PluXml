0d57bc599bf1831572fbd1be92d1055d95472d7c
Jean-Pierre Pourrez <kazimentou@free.fr>
2024-02-02 09:04:30 +0100

    Select template in the table of categories

--- core/admin/parametres_affichage.php
+++ core/admin/parametres_affichage.php
@@ -56,8 +56,7 @@ include 'top.php';
 
 	<div class="inline-form action-bar">
 		<h2><?= L_CONFIG_VIEW_FIELD ?></h2>
-		<p><?= L_CONFIG_VIEW_PLUXML_RESSOURCES ?></p>
-		<input type="submit" value="<?= L_CONFIG_VIEW_UPDATE ?>" />
+		<p><input type="submit" value="<?= L_CONFIG_VIEW_UPDATE ?>" /></p>
 	</div>
 
 	<?php eval($plxAdmin->plxPlugins->callHook('AdminSettingsDisplayTop')) # Hook Plugins ?>
