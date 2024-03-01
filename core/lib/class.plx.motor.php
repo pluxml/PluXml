a0e33c184d067d9cf59cda5936d6a2b241c807d2
Jean-Pierre Pourrez <kazimentou@free.fr>
2024-03-01 09:31:29 +0100

    Fix regular expression in plxAdmin::prechauffage()

--- core/lib/class.plx.motor.php
+++ core/lib/class.plx.motor.php
@@ -161,7 +161,7 @@ class plxMotor {
 		# Hook plugins
 		if(eval($this->plxPlugins->callHook('plxMotorPreChauffageBegin'))) return;
 
-		if(!empty($this->get) and !preg_match('#^(?:blog|article\d{1,4}/|static\d{1,3}/|categorie\d{1,3}/|user\d{1,3}|archives/\d{4}(?:/\d{2})?|tag/\w|page\d+|preview|telechargement|download)#', $this->get)) {
+		if(!empty($this->get) and !preg_match('#^(?:blog|article\d{1,4}|static\d{1,3}|categorie\d{1,3}|user\d{1,3}|archives/\d{4}(?:/\d{2})?|tag/\w+|page\d+|preview|telechargement|download)\b#', $this->get)) {
 			$this->get = '';
 		}
 
