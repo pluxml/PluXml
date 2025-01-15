af27b29738db2407fc8720153c2d99671db8bbfa
Jean-Pierre Pourrez <kazimentou@free.fr>
2025-01-15 10:50:22 +0100

    Add <div> in posts.php

--- themes/defaut/posts.php
+++ themes/defaut/posts.php
@@ -2,43 +2,45 @@
 if(!defined('PLX_ROOT')) {
 	exit;
 }
-
-while($plxShow->plxMotor->plxRecord_arts->loop()): ?>
-					<article class="article <?= $plxShow->artPinClass() ?>" id="post-<?= $plxShow->artId(); ?>">
-						<header>
-							<span class="art-date">
-								<time datetime="<?php $plxShow->artDate('#num_year(4)-#num_month-#num_day'); ?>">
-									<?php $plxShow->artDate('#num_day #month #num_year(4)'); ?>
-								</time>
-							</span>
-							<h2>
-								<?php $plxShow->artTitle('link'); ?>
-							</h2>
-							<div>
-								<small>
-									<span class="written-by">
-										<?php $plxShow->lang('WRITTEN_BY'); ?> <?php $plxShow->artAuthor() ?>
-									</span>
-									<span class="art-nb-com">
-										<?php $plxShow->artNbCom(); ?>
-									</span>
-								</small>
-							</div>
-							<div>
-								<small>
-									<span class="classified-in">
-										<?php $plxShow->lang('CLASSIFIED_IN') ?> : <?php $plxShow->artCat() ?>
-									</span>
-									<span class="tags">
-										<?php $plxShow->lang('TAGS') ?> : <?php $plxShow->artTags() ?>
-									</span>
-								</small>
-							</div>
-						</header>
-						<?php $plxShow->artThumbnail(); ?>
-						<?php $plxShow->artChapo(); ?>
-					</article>
+?>
+					<div class="article-loop">
+<?php while($plxShow->plxMotor->plxRecord_arts->loop()): ?>
+						<article class="article <?= $plxShow->artPinClass() ?>" id="post-<?= $plxShow->artId(); ?>">
+							<header>
+								<span class="art-date">
+									<time datetime="<?php $plxShow->artDate('#num_year(4)-#num_month-#num_day'); ?>">
+										<?php $plxShow->artDate('#num_day #month #num_year(4)'); ?>
+									</time>
+								</span>
+								<h2>
+									<?php $plxShow->artTitle('link'); ?>
+								</h2>
+								<div>
+									<small>
+										<span class="written-by">
+											<?php $plxShow->lang('WRITTEN_BY'); ?> <?php $plxShow->artAuthor() ?>
+										</span>
+										<span class="art-nb-com">
+											<?php $plxShow->artNbCom(); ?>
+										</span>
+									</small>
+								</div>
+								<div>
+									<small>
+										<span class="classified-in">
+											<?php $plxShow->lang('CLASSIFIED_IN') ?> : <?php $plxShow->artCat() ?>
+										</span>
+										<span class="tags">
+											<?php $plxShow->lang('TAGS') ?> : <?php $plxShow->artTags() ?>
+										</span>
+									</small>
+								</div>
+							</header>
+							<?php $plxShow->artThumbnail(); ?>
+							<?php $plxShow->artChapo(); ?>
+						</article>
 <?php endwhile; ?>
+					</div>
 					<nav class="pagination text-center">
 						<?php $plxShow->pagination(); ?>
 					</nav>
