6d3fe150e48cec397153bdf10d5584fa2fda1511
Jean-Pierre Pourrez <kazimentou@free.fr>
2025-08-24 11:21:51 +0200

    Enhance minify.php

--- themes/defaut/css/minify.php
+++ themes/defaut/css/minify.php
@@ -4,10 +4,22 @@
 const PLX_CORE = '../../../core/';
 include PLX_CORE . 'lib/class.plx.utils.php';
 
-foreach(array('plucss', 'theme',) as $f) {
-	echo $f . PHP_EOL;
-	$content = file_get_contents($f . '.css');
-	file_put_contents($f . '.min.css', plxUtils::minify($content));
+foreach(array('plucss', 'theme', 'print',) as $f) {
+	echo $f . ' ';
+	$src = $f . '.css';
+	if(!file_exists($src)) {
+		echo 'missing' . PHP_EOL;
+		continue;
+	}
+
+	$target = $f . '.min.css';
+	if(!file_exists($target) or filemtime($target) < filemtime($src)) {
+		$content = file_get_contents($src);
+		file_put_contents($target, plxUtils::minify($content));
+		echo 'updated' . PHP_EOL;
+	} else {
+		echo 'skipped' . PHP_EOL;
+	}
 }
 echo 'Done !' . PHP_EOL;
 
