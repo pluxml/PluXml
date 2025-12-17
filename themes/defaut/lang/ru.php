aea246486bd1273ceabdb78ff35b73101526766f
Jean-Pierre Pourrez <kazimentou@free.fr>
2025-12-17 23:29:48 +0100

    Fix russian translation

--- themes/defaut/lang/ru.php
+++ themes/defaut/lang/ru.php
@@ -3,53 +3,53 @@
 $LANG = array(
 
 #header.php
-'MENU'					=> 'меню',
+'MENU'					=> 'Меню',
 'HOME'					=> 'Главная',
-'GOTO_CONTENT'			=> 'Перейти к содержанию',
+'GOTO_CONTENT'			=> 'Перейти к содержимому',
 'GOTO_MENU'				=> 'Перейти к меню',
 'COMMENTS_RSS_FEEDS'	=> 'RSS-каналы комментариев',
 'COMMENTS'				=> 'Комментарии',
-'ARTICLES_RSS_FEEDS'	=> 'Rss-каналы статей',
+'ARTICLES_RSS_FEEDS'	=> 'RSS-каналы статей',
 'ARTICLES'				=> 'Статьи',
 
 # sidebar.php
 'CATEGORIES' 			=> 'Категории',
 'LATEST_ARTICLES'		=> 'Последние статьи',
 'LATEST_COMMENTS'		=> 'Последние комментарии',
-'ARCHIVES'				=> 'Архив',
+'ARCHIVES'				=> 'Архивы',
 
 # footer.php
-'POWERED_BY'			=> 'Работает на',
-'PLUXML_DESCRIPTION'	=> 'Блог и Cms без баз данных',
-'IN'					=> 'в',
-'ADMINISTRATION'		=> 'Администрация',
-'GOTO_TOP'				=> 'Перейти в верх',
-'TOP'					=> 'Вверх',
+'POWERED_BY'			=> 'Страница создана в',
+'PLUXML_DESCRIPTION'	=> 'Блог и CMS без баз данных',
+'IN'					=> 'за',
+'ADMINISTRATION'		=> 'Администрировать',
+'GOTO_TOP'				=> 'Перейти в начало страницы',
+'TOP'					=> 'Наверх',
 
 # erreur.php
-'ERROR'					=> 'Обнаружена ошибка',
+'ERROR'					=> 'Ошибка',
 'BACKTO_HOME'			=> 'Перейти на главную',
 
 # common
 'WRITTEN_BY'			=> 'Автор',
-'CLASSIFIED_IN'			=> 'Частные объявления на',
-'TAGS'					=> 'Тэги',
+'CLASSIFIED_IN'			=> 'Категории',
+'TAGS'					=> 'Метки',
 
 # commentaires.php
-'SAID'					=> 'сказал',
-'WRITE_A_COMMENT'		=> 'Создать комментарий',
+'SAID'					=> 'сказал(а)',
+'WRITE_A_COMMENT'		=> 'Прокомментировать',
 'NAME'					=> 'Имя',
-'WEBSITE'				=> 'сайта (необязательно)',
-'EMAIL'					=> 'E-mail (необязательно)',
+'WEBSITE'				=> 'Веб-сайт (необязательно)',
+'EMAIL'					=> 'Эл. почта (необязательно)',
 'COMMENT'				=> 'Комментарий',
 'CLEAR'					=> 'Очистить',
 'SEND'					=> 'Отправить',
-'COMMENTS_CLOSED'		=> 'Комментарии запрещенны',
+'COMMENTS_CLOSED'		=> 'Комментарии не принимаются',
 'ANTISPAM_WARNING'		=> 'Анти-спам проверка',
 
-'REPLY'					=> 'ответ',
-'REPLY_TO'				=> 'ответ',
-'CANCEL'				=> 'отменить',
+'REPLY'					=> 'Ответить',
+'REPLY_TO'				=> 'В ответ на',
+'CANCEL'				=> 'Отмена',
 
 );
 
